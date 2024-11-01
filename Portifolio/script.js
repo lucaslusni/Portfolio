89d3704cd309bf6abb91397bb2044329fabc70f4
Lucas Lusni <lucaslusni@gmail.com>
2024-11-01 03:47:06 -0300

    Portifolio
    portifolio

---
+++ Portifolio/script.js
@@ -0,0 +1,10 @@
+// Scroll suave para as seções
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
